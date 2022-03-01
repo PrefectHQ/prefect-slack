f4a94f6891b00010d419fc6d2c03ab530c08bd90
Alexander Streed <alex.s@prefect.io>
2022-03-01 15:01:19 -0600

    Imports AsyncMock from prefect.utilities.testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,13 +1,7 @@
-import sys
 from unittest.mock import MagicMock
 
 import pytest
-
-if sys.version_info < (3, 8):
-    # https://docs.python.org/3/library/unittest.mock.html#unittest.mock.AsyncMock
-    from mock import AsyncMock
-else:
-    from unittest.mock import AsyncMock
+from prefect.utilities.testing import AsyncMock
 
 
 @pytest.fixture
