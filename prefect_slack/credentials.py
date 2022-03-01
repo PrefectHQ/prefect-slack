e321317af3bb353f71c05b741f89a5a4f6cf9ad1
Alexander Streed <alex.s@prefect.io>
2022-03-01 10:53:19 -0600

    Adds docuemntation for credentials module

--- prefect_slack/credentials.py
+++ prefect_slack/credentials.py
@@ -5,7 +5,16 @@ from slack_sdk.web.async_client import AsyncWebClient
 
 @dataclass
 class SlackCredentials:
+    """
+    Class for holding Slack credentials for use in tasks and flows.
+
+    Args:
+        token: Bot user OAuth token for the Slack app used to perform actions.
+    """
     token: str
 
     def get_slack_web_client(self) -> AsyncWebClient:
+        """
+        Returns an authenticated `AsyncWebClient` to interact with the Slack API.
+        """
         return AsyncWebClient(token=self.token)
