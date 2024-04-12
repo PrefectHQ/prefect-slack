1ef9eb5deac6f6d3d4d36ae567665a54da763a54
Chris Guidry <chris.g@prefect.io>
2024-04-12 16:43:07 -0400

    Introducing conditional support for `NotificationBlock.raise_on_failure`
    In prefect>=2.17.2, we'll have a new capability to instruct notification blocks
    to raise errors when they have a problem sending.  Here, we use an duck-typing
    and conditional import approach to testing for this in the SlackWebhook block.

--- tests/test_credentials.py
+++ tests/test_credentials.py
@@ -1,5 +1,8 @@
+from unittest.mock import AsyncMock
+
+import pytest
 from slack_sdk.web.async_client import AsyncWebClient
-from slack_sdk.webhook.async_client import AsyncWebhookClient
+from slack_sdk.webhook.async_client import AsyncWebhookClient, WebhookResponse
 
 from prefect_slack import SlackCredentials, SlackWebhook
 
@@ -13,3 +16,51 @@ def test_slack_webhook():
         SlackWebhook(url="https://hooks.slack.com/xxxx").get_client(),
         AsyncWebhookClient,
     )
+
+
+@pytest.mark.skipif(
+    condition=not hasattr(SlackWebhook, "raise_on_failure"),
+    reason="The raise_on_failure option is only implemented in prefect>=2.17.2",
+)
+async def test_slack_webhook_block_does_not_raise_on_success(
+    monkeypatch: pytest.MonkeyPatch,
+):
+    monkeypatch.setattr(
+        AsyncWebhookClient,
+        "send",
+        AsyncMock(
+            return_value=WebhookResponse(
+                url="http://wherever", status_code=222, body="yay", headers={}
+            )
+        ),
+    )
+    block = SlackWebhook(url="http://wherever")
+
+    with block.raise_on_failure():
+        await block.notify("hello", "world")
+
+
+@pytest.mark.skipif(
+    condition=not hasattr(SlackWebhook, "raise_on_failure"),
+    reason="The raise_on_failure option is only implemented in prefect>=2.17.2",
+)
+async def test_slack_webhook_block_handles_raise_on_failure(
+    monkeypatch: pytest.MonkeyPatch,
+):
+    monkeypatch.setattr(
+        AsyncWebhookClient,
+        "send",
+        AsyncMock(
+            return_value=WebhookResponse(
+                url="http://wherever", status_code=400, body="woops", headers={}
+            )
+        ),
+    )
+
+    from prefect.blocks.abstract import NotificationError
+
+    block = SlackWebhook(url="http://wherever")
+
+    with pytest.raises(NotificationError, match="Failed to send message: woops"):
+        with block.raise_on_failure():
+            await block.notify("hello", "world")
